65cba86335fa1d152c0789011ec9d6804b1367ec
thirdfree2 <126750481+thirdfree2@users.noreply.github.com>
2023-09-09 00:43:37 +0700

    aadd

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const flash = require('express-flash');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
 const dbCon = require("./lib/db");
+
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const psychonistRouter = require('./routes/psychonist');
+const exp = require('constants');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-const jwt = require('jsonwebtoken');
-// Replace 'your-secret-key' with your actual secret key
-const secretKey = process.env.JWT_SECRET || 'your-secret-key';
 
 
 app.use(logger('dev'));
@@ -40,6 +40,27 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/psychonist', psychonistRouter);
 
+const secretKey = 'your_secret_key';
+
+
+// Middleware to verify the token
+function verifyToken(req, res, next) {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token not provided' });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    req.userId = decoded.user_id;
+    next();
+  });
+}
+
 // Get psychologists
 app.get('/getpsychonist', (req, res) => {
   dbCon.query('SELECT * FROM doc_user_table', (error, results, fields) => {
@@ -56,34 +77,156 @@ app.get('/getpsychonist', (req, res) => {
   });
 });
 
-// User registration
-app.post('/signup', async (req, res) => {
-  const { user_name, user_email, user_password, user_phone } = req.body;
+app.get('/getuser', (req, res) => {
+  dbCon.query('SELECT * FROM user_table', (error, results, fields) => {
+    if (error) {
+      console.error('Error while fetching psychologists from the database:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    if (results === undefined || results.length === 0) {
+      return res.json({ error: false, data: [], message: 'Empty' });
+    }
+
+    return res.json({ error: false, data: results, message: 'Success' });
+  });
+});
+
+
+//sign up
+app.post('/registion', async (req, res) => {
+  const { user_password, user_email, user_phone, user_name } = req.body;
+
+  // Check if the 'password' field is missing or null/empty
+  if (!user_password) {
+    return res.status(400).json({ message: "Password is required" });
+  }
 
   try {
-    const hashedPassword = await bcrypt.hash(user_password, 10); // Hash the user's password
+
+    const hashedPassword = await bcrypt.hash(user_password, 10);
 
     dbCon.query(
-      'INSERT INTO user_table (user_name, user_email, user_password, user_phone) VALUES (?, ?, ?, ?)',
-      [user_name, user_email, hashedPassword, user_phone],
-      (err, results) => {
+      "INSERT INTO user_phone(user_password, user_email, user_phone, user_name) VALUES(?,?,?,?)",
+      [hashedPassword, user_email, user_phone, user_name],
+      (err, results, fields) => {
         if (err) {
-          console.error('Error while inserting a user into the database:', err);
-          return res.status(400).json({ error: 'Registration failed' });
+          console.log("Error : ", err);
+          return res.status(400).send();
+        }
+        return res.status(201).json({ message: "Success" });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
+});
+
+
+app.post('/login', async (req, res) => {
+  const { user_name, user_password } = req.body;
+
+  try {
+    dbCon.query(
+      'SELECT * FROM user_phone WHERE user_name = ?',
+      [user_name],
+      async (err, results, fields) => {
+        if (err) {
+          console.error('Error:', err);
+          return res.status(400).send();
         }
 
-        console.log('User registered successfully');
-        return res.status(201).json({ message: 'New user successfully registered' });
+        if (results.length === 0) {
+          return res.status(401).json({ message: 'User not found' });
+        }
+
+        const user = results[0];
+        const passwordMatch = await bcrypt.compare(user_password, user.user_password);
+
+        if (!passwordMatch) {
+          return res.status(401).json({ message: 'Invalid password' });
+        }
+
+        // Generate a JWT token upon successful login
+        const token = jwt.sign({ user_id: user.id }, secretKey, { expiresIn: '1h' });
+
+        return res.status(200).json({ message: 'Login successful', token });
       }
     );
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ error: 'Internal server error' });
+    return res.status(500).send();
   }
 });
 
 
 
+app.post('/registionaddmin', async (req, res) => {
+  const { admin_password, admin_email,admin_user } = req.body;
 
+  // Check if the 'password' field is missing or null/empty
+  if (!admin_password) {
+    return res.status(400).json({ message: "Password is required" });
+  }
+
+  try {
+
+    const hashedPassword = await bcrypt.hash(admin_password, 10);
+
+    dbCon.query(
+      "INSERT INTO admin_table(admin_password, admin_email,admin_user) VALUES(?,?,?)",
+      [hashedPassword, admin_email, admin_user],
+      (err, results, fields) => {
+        if (err) {
+          console.log("Error : ", err);
+          return res.status(400).send();
+        }
+        return res.status(201).json({ message: "Success" });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
+});
+
+
+
+app.post('/adminlogin', async (req, res) => {
+  const { admin_user, admin_password } = req.body;
+
+  try {
+    dbCon.query(
+      'SELECT * FROM admin_table WHERE admin_user = ?',
+      [admin_user],
+      async (err, results, fields) => {
+        if (err) {
+          console.error('Error:', err);
+          return res.status(400).send();
+        }
+
+        if (results.length === 0) {
+          return res.status(401).json({ message: 'admin not found' });
+        }
+
+        const admin = results[0];
+        const passwordMatch = await bcrypt.compare(admin_password, admin.admin_password);
+
+        if (!passwordMatch) {
+          return res.status(401).json({ message: 'Invalid password' });
+        }
+
+        // Generate a JWT token upon successful login
+        const token = jwt.sign({ admin_id: admin.id }, secretKey, { expiresIn: '1h' });
+
+        return res.redirect('/psychonist');
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send();
+  }
+});
 
 module.exports = app;
