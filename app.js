4622efb6b1747e46703e8b2078221325178df19a
thirdfree2 <pativeth.k@gmail.com>
2023-09-08 16:45:44 +0700

    aa

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const flash = require('express-flash');
 const session = require('express-session');
-const mysql = require('mysql');
 const bcrypt = require('bcrypt');
-const util = require('util'); // Import the util module
-
 const dbCon = require("./lib/db");
 
 const indexRouter = require('./routes/index');
@@ -17,9 +13,12 @@ const psychonistRouter = require('./routes/psychonist');
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+const jwt = require('jsonwebtoken');
+// Replace 'your-secret-key' with your actual secret key
+const secretKey = process.env.JWT_SECRET || 'your-secret-key';
 
 
 app.use(logger('dev'));
@@ -29,46 +28,59 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  cookie: { maxAge: 60000},
-  store: new session.MemoryStore,
-  saveUninitialized: 'true',
+  cookie: { maxAge: 60000 },
+  store: new session.MemoryStore(),
+  saveUninitialized: true, // Set to true without quotes
   secret: 'secret'
-}))
+}));
 
 app.use(flash());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/psychonist', psychonistRouter)
+app.use('/psychonist', psychonistRouter);
 
-
-
-
-// get psychonist
-app.get('/getpsychonist',(req,res) => {
+// Get psychologists
+app.get('/getpsychonist', (req, res) => {
   dbCon.query('SELECT * FROM doc_user_table', (error, results, fields) => {
-      if(error) throw error;
-      let message = ""
-      if(results === undefined || results.length == 0){
-          message = "Empty";
-      } else {
-          message = "Success";
-      }
-      return res.send({error: false, data:results, message:message})
-  })
-})
+    if (error) {
+      console.error('Error while fetching psychologists from the database:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
 
-app.get('/user',(req,res) => {
-  dbCon.query('SELECT * FROM user_table', (error, results, fields) => {
-      if(error) throw error;
-      let message = ""
-      if(results === undefined || results.length == 0){
-          message = "Empty";
-      } else {
-          message = "Success";
+    if (results === undefined || results.length === 0) {
+      return res.json({ error: false, data: [], message: 'Empty' });
+    }
+
+    return res.json({ error: false, data: results, message: 'Success' });
+  });
+});
+
+// User registration
+app.post('/signup', async (req, res) => {
+  const { user_name, user_email, user_password, user_phone } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(user_password, 10); // Hash the user's password
+
+    dbCon.query(
+      'INSERT INTO user_table (user_name, user_email, user_password, user_phone) VALUES (?, ?, ?, ?)',
+      [user_name, user_email, hashedPassword, user_phone],
+      (err, results) => {
+        if (err) {
+          console.error('Error while inserting a user into the database:', err);
+          return res.status(400).json({ error: 'Registration failed' });
+        }
+
+        console.log('User registered successfully');
+        return res.status(201).json({ message: 'New user successfully registered' });
       }
-      return res.send({error: false, data:results, message:message})
-  })
-})
+    );
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 
 
